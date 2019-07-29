5a51d2e38680d7a2de74f3c47f2f0b483e405c1e
fumiya <fu38.terada@gmail.com>
2019-07-29 22:09:51 +0900

    first commit
    html、jsファイルを作成

---
+++ app.js
@@ -0,0 +1,17 @@
+let array = [];
+
+let i = [];
+i[0] = 0;
+i[1] = 1;
+i[2] = 2;
+i[3] = 3;
+i[4] = 4;
+i[5] = 5;
+
+array.push(i);
+
+console.log(array[0][0]);
+console.log(array[0][1]);
+console.log(array[0][3]);
+console.log(array[0][4]);
+console.log(array[0][5]);
